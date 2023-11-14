a4470833b23323df47a00c11f132896eb880435f
Andrés Lapo <jandlapomotmail.com>
2023-11-13 21:00:07 -0500

    Adicion del DAO y arreglo de bugs

---
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -0,0 +1,33 @@
+package com.latam.alura.tienda.prueba;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import com.latam.alura.tienda.dao.CategoriaDAO;
+import com.latam.alura.tienda.dao.ProductoDAO;
+import com.latam.alura.tienda.modelo.Categoria;
+import com.latam.alura.tienda.modelo.Producto;
+import com.latam.alura.tienda.utils.JPAUtils;
+
+public class RegistroDeProducto {
+
+	public static void main(String[] args) {
+		Categoria celulares = new Categoria("CELULARES");
+		Producto celular = new Producto("Samsung", "telefono usado",new BigDecimal("1000"),celulares);
+		
+		EntityManager em = JPAUtils.getEntityManager();
+		
+		ProductoDAO productoDao= new ProductoDAO(em);
+		CategoriaDAO categoriaDao = new CategoriaDAO(em);
+		
+		em.getTransaction().begin();
+		
+		categoriaDao.guardar(celulares);
+		productoDao.guardar(celular);
+		
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
